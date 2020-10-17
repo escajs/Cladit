39ae2a411efef793e0356dc68f8b3315348ce4f6
escajs <62265728+escajs@users.noreply.github.com>
2020-10-17 22:26:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(()=>{
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+window.addEventListener('scroll',function(){
+    var nav=document.getElementById('navuri');
+    var navItems=document.querySelector('#nav-items');
+    nav.classList.toggle('sticky',window.scrollY >30);
+    //navItems.classList.toggle('red-text',window.scrollY >30);
+    
+})
+  
+})
